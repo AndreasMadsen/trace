8ccc40cf2148b1f4200abb4cda1f8c7a65433025
Andreas Madsen <amwebdk@gmail.com>
2014-10-22 23:34:33 +0200

    [fix] test in node 0.11.14

--- test/regression/httpClose.js
+++ test/regression/httpClose.js
@@ -11,11 +11,11 @@ test("test an issue with http where events wheren't removed", function test(t) {
 
   http.createServer(function (req, res) {
     res.end('ho away :(');
-  }).listen(0, function listen() {
+  }).listen(0, 'localhost', function listen() {
     var self = this;
     var addr = this.address();
 
-    http.get('http://' + addr.address + ':' + addr.port, function get(res) {
+    http.get('http://localhost:' + addr.port, function get(res) {
       res.on('data', function () {});
       res.once('end', function end() {
 
@@ -26,6 +26,7 @@ test("test an issue with http where events wheren't removed", function test(t) {
             "at producer (./test/produce.js:16:24)",
             "at Server.close (./test/regression/httpClose.js:24:23)",
             "at IncomingMessage.end (./test/regression/httpClose.js:22:14)",
+            "at ClientRequest.get (./test/regression/httpClose.js:19:11)",
             "at Server.listen (./test/regression/httpClose.js:18:10)",
             "at Test.test (./test/regression/httpClose.js:14:6)",
             "at Object.<anonymous> (./test/regression/httpClose.js:10:1)"
