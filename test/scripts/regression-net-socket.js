ad13f43176f2439704346b5bd2875995acddf3eb
Andreas Madsen <amwebdk@gmail.com>
2015-10-15 14:49:23 +0200

    [test] add net socket regression

---
+++ test/scripts/regression-net-socket.js
@@ -0,0 +1,15 @@
+'use strict';
+require('../../trace.js');
+
+const net = require('net');
+
+const server = net.createServer(function (socket) {
+  throw new Error('trace');
+});
+
+server.listen(0, 'localhost', function () {
+  const addr = server.address();
+  const socket = net.connect(addr.port, addr.address, function () {
+    // just hang, it will throw
+  });
+});
