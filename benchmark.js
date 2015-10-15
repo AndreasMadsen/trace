6e7a66174ab628ffbbcbbf5891084d31c00ffb91
Andreas Madsen <amwebdk@gmail.com>
2015-10-15 15:22:16 +0200

    [cleanup] benchmark

--- benchmark.js
+++ benchmark.js
@@ -1,54 +1,54 @@
-
-var summary = require('summary');
+'use strict';
+const summary = require('summary');
 
 function timeit(top, callback) {
-    var times = new Float64Array(top);
-    (function recursive(i) {
-        var tick = process.hrtime();
-        setImmediate(function () {
-            if (i === top) {
-                callback(summary(times));
-            } else {
-                var tock = process.hrtime(tick);
-                times[i] = tock[0] * 1e9 + tock[1];
+  const times = new Float64Array(top);
+  (function recursive(i) {
+    const tick = process.hrtime();
+    setImmediate(function () {
+      if (i === top) {
+        callback(summary(times));
+      } else {
+        const tock = process.hrtime(tick);
+        times[i] = tock[0] * 1e9 + tock[1];
 
-                recursive(i + 1);
-            }
-        });
-    })(0);
+        recursive(i + 1);
+      }
+    });
+  })(0);
 }
 
 ({
-    'master': function () {
-        var fork = require('child_process').fork;
+  'master': function () {
+    const fork = require('child_process').fork;
 
-        function bench(name, callback) {
-            var cp = fork(__filename, [name]);
-            cp.once('message', function (stat) {
-                console.log(name + ': ' + stat.mean.toFixed(4) + ' ± ' + (1.96 * stat.sd).toFixed(4) + ' ns/tick');
-            });
-            cp.once('close', callback);
-        }
-
-        bench('baseline', function () {
-            bench('trace', function () {
-                console.log('done');
-            });
-        });
-    },
-
-    'baseline': function () {
-        var top = 100000;
-        timeit(top, function (stat) {
-            process.send({ "mean": stat.mean(), "sd": stat.sd() });
-        });
-    },
-
-    'trace': function () {
-        require('./trace.js');
-        var top = 50000;
-        timeit(top, function (stat) {
-            process.send({ "mean": stat.mean(), "sd": stat.sd() });
-        });
+    function bench(name, callback) {
+      const cp = fork(__filename, [name]);
+      cp.once('message', function (stat) {
+          console.log(name + ': ' + stat.mean.toFixed(4) + ' ± ' + (1.96 * stat.sd).toFixed(4) + ' ns/tick');
+      });
+      cp.once('close', callback);
     }
+
+    bench('baseline', function () {
+      bench('trace', function () {
+        console.log('done');
+      });
+    });
+  },
+
+  'baseline': function () {
+    const top = 500000;
+    timeit(top, function (stat) {
+      process.send({ 'mean': stat.mean(), 'sd': stat.sd() });
+    });
+  },
+
+  'trace': function () {
+    require('./trace.js');
+    const top = 100000;
+    timeit(top, function (stat) {
+      process.send({ 'mean': stat.mean(), 'sd': stat.sd() });
+    });
+  }
 })[process.argv[2] || 'master']();
