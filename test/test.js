b3a8382a6099da0a1bc494d943306c0ab25f1abc
James Bunton <jbunton@atlassian.com>
2018-02-06 17:07:11 +1100

    [test] Ignore stack lines from outside our test code
    This stops the tests from breaking with Node.js updates.

--- test/test.js
+++ test/test.js
@@ -19,14 +19,14 @@ interpreted({
     const p = execspawn(`${process.execPath} -r ../trace.js --stack-trace-limit=1000 ${JSON.stringify(filepath)} 2>&1`);
     p.stdout.pipe(endpoint(function (err, output) {
       if (err) return callback(err);
-      callback(null, stripPosInfo(stripPath(output.toString('ascii'))));
+      callback(null, prepareString(output.toString('ascii')));
     }));
   },
 
   types: {
     'txt': {
       test: function (t, actual, expected) {
-        t.strictEqual(actual, expected);
+        t.strictEqual(actual.trim(), expected.trim());
       },
       update: function (actual) {
         return actual;
@@ -35,8 +35,24 @@ interpreted({
   }
 });
 
-function stripPath(output) {
-  return output.split(__dirname).join('');
+function prepareString(text) {
+  const lines = stripPosInfo(text).split('\n');
+
+  const processedLines = lines.map((line) => {
+    const isTraceLine = line.match(/:[0-9]+:[0-9]+/);
+    const isFromTestScript = line.includes(__dirname);
+
+    const processedLine = line.replace(__dirname, '') + '\n';
+
+    if (isTraceLine && !isFromTestScript) {
+      // Only include stack trace lines from our test code.
+      // This stops the tests from breaking whenever we change Node.js version.
+      return '';
+    }
+    return processedLine;
+  });
+
+  return processedLines.join('');
 }
 
 function stripPosInfo(text) {
