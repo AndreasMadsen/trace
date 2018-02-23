c6f4efff2b3694920d2f18ac5d9b0384073e1ab9
James Bunton <jbunton@atlassian.com>
2018-02-23 13:33:54 +1100

    [fix] copy the important fields from CallSite instead of keeping the original
    This stops a memory leak. The CallSite keeps a reference to Promise
    objects. This stops the Promise from being garbage collected, which
    stops asyncDestroy from being called.

--- trace.js
+++ trace.js
@@ -50,7 +50,39 @@ function getCallSites(skip) {
   });
   Error.stackTraceLimit = limit;
 
-  return stack;
+  return copyFrames(stack);
+}
+
+// We must take a copy of the CallSite objects to avoid retaining references to Promises.
+// If we retain a Promise reference then asyncDestroy for the Promise won't be called,
+// so we'll leak memory.
+class CallSiteCopy {
+  constructor(callSite) {
+    this._fileName = callSite.getFileName();
+    this._lineNumber = callSite.getLineNumber();
+    this._columnNumber = callSite.getColumnNumber();
+    this._toString = callSite.toString(); // TODO this is slow
+  }
+
+  getFileName() {
+    return this._fileName;
+  }
+
+  getLineNumber() {
+    return this._lineNumber;
+  }
+
+  getColumnNumber() {
+    return this._columnNumber;
+  }
+
+  toString() {
+    return this._toString;
+  }
+}
+
+function copyFrames(frames) {
+  return frames.map((callSite) => new CallSiteCopy(callSite));
 }
 
 function equalCallSite(a, b) {
