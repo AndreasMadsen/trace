10698105c2b68add47013ebb352093ffa37d7fcd
James Bunton <jbunton@atlassian.com>
2018-02-23 14:08:59 +1100

    [debug] pretty print async context tree when extending a stack trace

--- debug.js
+++ debug.js
@@ -6,6 +6,54 @@ function debug(msg) {
   fs.writeSync(1, 'trace.js DEBUG ' + msg + '\n');
 }
 
+function printRootTraces(traces) {
+  const rootAsyncIds = findRootAsyncIds(traces);
+  for (const asyncId of rootAsyncIds) {
+    printTree(traces, traces.get(asyncId));
+  }
+}
+
+function findRootAsyncIds(traces) {
+  const asyncIds = new Set(traces.keys());
+  for (const trace of traces.values()) {
+    for (const notRootTrace of trace.relatedTraces) {
+      asyncIds.delete(notRootTrace.asyncId);
+    }
+  }
+  return asyncIds;
+}
+
+function printTree(traces, trace, indent='', isLast=true, visited=new Set()) {
+  let line = indent + '\\-' + trace.asyncId;
+
+  if (isLast) {
+    indent += ' ';
+  } else {
+    indent += '| ';
+  }
+
+  if (!traces.get(trace.asyncId)) {
+    line += ' (not-root)';
+  }
+
+  if (visited.has(trace.asyncId)) {
+    line += ' (cycle)';
+  }
+
+  fs.writeSync(1, line + '\n');
+
+  if (visited.has(trace.asyncId)) {
+    return;
+  }
+  visited.add(trace.asyncId);
+
+  for (let i = 0; i < trace.relatedTraces.length; ++i) {
+    const isLast = i === trace.relatedTraces.length - 1;
+    printTree(traces, trace.relatedTraces[i], indent, isLast, visited);
+  }
+}
+
 module.exports = {
-  debug
+  debug,
+  printRootTraces
 };
