2889e9420898f8582fafe9f5cbcd4e01967b7e91
Alejandro Fernández Haro <afharo@gmail.com>
2024-10-23 03:19:44 +0200

    Add support for Node.js v20+ (#57)
    1. Internals are now prefixed with node:internal/
    2. Node.js prints it's version during exceptions, strip this information.
    3. Update test expectations.
    
    Co-authored-by: Andreas Madsen <amwebdk@gmail.com>

--- test/test.js
+++ test/test.js
@@ -4,22 +4,25 @@ const interpreted = require('interpreted');
 const execspawn = require('execspawn');
 const endpoint = require('endpoint');
 const path = require('path');
+const process = require('process');
 
+const TRACE_PATH = path.resolve(__dirname, '../trace.js');
 const SCRIPTS_PATH = path.resolve(__dirname, 'scripts');
+const EXPECTED_PATH = path.resolve(__dirname, 'expected');
 
 interpreted({
   source: SCRIPTS_PATH,
-  expected: path.resolve(__dirname, 'expected'),
+  expected: EXPECTED_PATH,
   readSource: false,
 
   update: false,
 
   test: function (name, callback) {
     const filepath = path.join(SCRIPTS_PATH, name + '.js');
-    const p = execspawn(`${process.execPath} -r ../trace.js --stack-trace-limit=1000 ${JSON.stringify(filepath)} 2>&1`);
+    const p = execspawn(`${process.execPath} -r ${TRACE_PATH} --stack-trace-limit=1000 ${JSON.stringify(filepath)} 2>&1`);
     p.stdout.pipe(endpoint(function (err, output) {
       if (err) return callback(err);
-      callback(null, stripPosInfo(stripPath(output.toString('ascii'))));
+      callback(null, stripNodeVersion(stripPosInfo(stripPath(output.toString('ascii')))));
     }));
   },
 
@@ -41,4 +44,8 @@ function stripPath(output) {
 
 function stripPosInfo(text) {
   return text.replace(/:[0-9]+:[0-9]+/g, ':r:c');
+}
+
+function stripNodeVersion(text) {
+  return text.replace(`Node.js ${process.version}`, 'Node.js vx.y.z');
 }
